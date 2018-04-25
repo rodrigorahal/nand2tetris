50899bed939164e6bc2c0aca1ef6e0cf1eee5706
rodrigorahal <rodrigo@livup.com.br>
2018-04-24 22:46:14 -0300

    add mult assembly program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+    @i    // i refers to some mem. location
+    M=1   // i=1
+    @R2   // sum
+    M=0   // R2=0
+
+(LOOP)
+
+    @i
+    D=M     // D=i
+
+    @R0
+    D=D-M   // D=i-R0
+
+    @END
+    D;JGT   // if (i-R0)>0 goto END
+
+    @R1
+    D=M    // D=R1
+
+    @R2
+    M=D+M   // sum=sum+R2
+
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP   // Infinite Loop
